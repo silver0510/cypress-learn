93654c96952ba15473a6b328d0bd386b7f681458
nghiapp7018 <nghia.pham@co-well.vn>
2022-10-03 22:43:23 +0700

    add home page testing

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,7 @@
+describe("Newsletter Subscribe Form", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000")
+    })
+
+    it("allows users to subscribe to the email list", () => { })
+})
