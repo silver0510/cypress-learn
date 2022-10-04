f3fc5d93a2a4b633e86e872e815fe0b941bb4afe
NghiaPP <phunghia91bkdn@gmail.com>
2022-10-04 15:01:38 +0700

    update test for subscribe

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -3,5 +3,24 @@ describe("Newsletter Subscribe Form", () => {
         cy.visit("http://localhost:3000")
     })
 
-    it("allows users to subscribe to the email list", () => { })
+    it("Allows users to subscribe to the email list", () => {
+        var email = "tom@aol.com"
+        cy.getByData("email-input").type(email)
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains(email)
+    })
+
+    it("Does NOT allow an invalid email address", () => {
+        cy.getByData("email-input").type("tom")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+    })
+
+    it("Does NOT allow already subscribed email addresses", () => {
+        var email = "john@example.com"
+        cy.getByData("email-input").type(email)
+        cy.getByData("submit-button").click()
+        cy.getByData("server-error-message").should("exist").contains(email)
+    })
+
 })
